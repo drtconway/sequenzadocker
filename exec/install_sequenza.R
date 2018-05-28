f6b995458ff88b4c7e26e55df6cf39f86967d761
Francesco Favero <francesco.favero@bric.ku.dk>
2018-05-28 12:48:04 +0200

    update sequenza to cleanup

--- exec/install_sequenza.R
+++ exec/install_sequenza.R
@@ -1,6 +1,14 @@
-packages <- c("sequenza")
+#!/services/tools/R-3.2.1/bin/R
+
+
+packages <- c("devtools", "readr", "iotools", "seqminer")
 
 setRepositories(graphics = FALSE, ind = 1:6)
 chooseCRANmirror(graphics = FALSE, ind = 2)
 
 install.packages(packages)
+
+library(devtools) 
+
+install_git("https://bitbucket.org/sequenza_tools/sequenza.git",
+    branch="cleanup")
