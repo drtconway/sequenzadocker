0261862caf9b407f244594b4fa64bb58b42038f2
Francesco Favero <francesco.favero@bric.ku.dk>
2018-05-28 22:05:55 +0200

    Add copynumber as dependencies

--- exec/install_sequenza.R
+++ exec/install_sequenza.R
@@ -1,7 +1,7 @@
 #!/services/tools/R-3.2.1/bin/R
 
 
-packages <- c("readr", "iotools", "seqminer", "devtools")
+packages <- c("readr", "iotools", "seqminer", "devtools", "copynumber")
 
 setRepositories(graphics = FALSE, ind = 1:6)
 chooseCRANmirror(graphics = FALSE, ind = 2)
